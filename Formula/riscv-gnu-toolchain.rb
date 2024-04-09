5f42b975256d4a2b003192db4c88d5e39e28888f
Matthias Kübrich <matthias.kuebrich@t-online.de>
2024-04-09 23:35:04 +0200

    Add dependencies to riscv-gnu-toolchain

--- Formula/riscv-gnu-toolchain.rb
+++ Formula/riscv-gnu-toolchain.rb
@@ -4,21 +4,35 @@ class RiscvGnuToolchain < Formula
   license "NOASSERTION"
   head "https://github.com/riscv-collab/riscv-gnu-toolchain.git"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "bc" => :build
+  depends_on "bison" => :build
+  depends_on "cmake" => :build
+  depends_on "curl" => :build
   depends_on "expat" => :build
+  depends_on "flex" => :build
   depends_on "flock" => :build
   depends_on "gawk" => :build
+  depends_on "gawk" => :build
+  depends_on "git" => :build
+  depends_on "glib" => :build
   depends_on "gmp" => :build
   depends_on "gnu-sed" => :build
+  depends_on "gperf" => :build
   depends_on "isl" => :build
   depends_on "libmpc" => :build
+  depends_on "libtool" => :build
   depends_on "mpfr" => :build
+  depends_on "ninja" => :build
+  depends_on "patchutils" => :build
   depends_on "python3" => :build
+  depends_on "texinfo" => :build
   depends_on "texinfo" => :build
   depends_on "zlib" => :build
 
   def install
     system "./configure", "--with-arch=rv32i", "--with-abi=ilp32", *std_configure_args
     system "make"
-    system "make", "install"
   end
 end
