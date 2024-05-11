80bb035420741d6f435d319a4133075ee1bdf7f0
Matthias Kübrich <matthias.kuebrich@t-online.de>
2024-05-11 21:59:31 +0200

    Fix jekyll

--- Formula/jekyll.rb
+++ Formula/jekyll.rb
@@ -1,14 +1,15 @@
 class Jekyll < Formula
   desc "Blog-aware static site generator in Ruby"
   homepage "https://jekyllrb.com"
-  url "https://github.com/jekyll/jekyll/archive/refs/tags/v4.3.3.tar.gz"
-  sha256 "61903b4c731557cdca489e381e509ea113258a2b88426b8c20b3bc4c7145d507"
+  url "https://github.com/jekyll/jekyll.git",
+    tag: "v4.3.3"
   license "MIT"
 
-  depends_on "ruby" => :build
+  depends_on "ruby"
 
   def install
+    ENV["GEM_HOME"] = "#{prefix}"
     system "gem", "build", "#{name}.gemspec"
-    bin.install "exe/#{name}"
+    system "gem", "install", "jekyll-#{version}.gem"
   end
 end
